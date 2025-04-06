63bcd4c903b9559dd084a057a3a19c24065d1799
Saketkumar24 <saketc112@gmail.com>
2025-04-07 03:36:19 +0530

    s

--- server/utils/index.js
+++ server/utils/index.js
@@ -4,12 +4,13 @@ const createJWT = (res, userId) => {
     expiresIn: "1d",
   });
 
-  res.cookie("token", token, {
+  res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
-    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'None',
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
   });
+  
 };
 
 export default createJWT;
