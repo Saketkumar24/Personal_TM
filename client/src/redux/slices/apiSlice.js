8831493b58a61c30e59f8c50fe8e41198b385eef
Saketkumar24 <saketc112@gmail.com>
2025-04-07 02:54:28 +0530

    s

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-// const API_URL = "http://localhost:8800/api";
 
-
-const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: API_URL,
+  credentials: "include", // ✅ this allows sending cookies with every request
+});
 
 export const apiSlice = createApi({
   baseQuery,
