83babf265e089cdb9aa54d616c38f0a3d37c564e
Saketkumar24 <saketc112@gmail.com>
2025-04-07 16:54:53 +0530

    s

--- server/index.js
+++ server/index.js
@@ -3,33 +3,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-
 import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewares.js";
 import routes from "./routes/index.js";
-import taskRoutes from "./routes/taskRoutes.js";
 import dbConnection from "./utils/connectDB.js";
+import taskRoutes from './routes/taskRoutes.js';
+
+
 
 dotenv.config();
+
 dbConnection();
 
 const PORT = process.env.PORT || 8800;
+
 const app = express();
 
-// For __dirname with ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://prismatic-lokum-aadc9d.netlify.app",
-      "https://personal-tm-frontend.onrender.com",
-    ],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://prismatic-lokum-aadc9d.netlify.app/"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
@@ -37,29 +28,14 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
-// app.use(morgan("dev"));
 
-// Routes
+//app.use(morgan("dev"));
 app.use("/api", routes);
-app.use("/api/task", taskRoutes); // <- must be before error handlers
 
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
-
-// Error handlers
 app.use(routeNotFound);
 app.use(errorHandler);
 
-// Start server
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+app.use("/api/task", taskRoutes);
