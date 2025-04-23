6ab693eb7cfeba441c9f4ce5f122cc58ad9d26a4
Saketkumar24 <saketc112@gmail.com>
2025-04-23 21:28:01 +0530

    3

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
 const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "1d",
   });
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: true, // Use secure cookies in production
     samesite: "none",
-   maxAge: 1 * 24 * 60 * 60 * 1000, // 7 days
+   maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
 
