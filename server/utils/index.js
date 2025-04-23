a156241625742f6e340c7a445d163ed11a1b4faa
Saketkumar24 <saketc112@gmail.com>
2025-04-23 21:23:49 +0530

    3

--- server/utils/index.js
+++ server/utils/index.js
@@ -7,7 +7,8 @@ const createJWT = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true, // Use secure cookies in production
-   maxAge: 7 * 24 * 60 * 60 * 1000, // 1 days
+    samesite: "none",
+   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
 
