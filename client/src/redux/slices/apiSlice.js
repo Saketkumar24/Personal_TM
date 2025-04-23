b3ce56b10607224710d017437d24f416f00fe6ab
Saketkumar24 <saketc112@gmail.com>
2025-04-23 19:53:17 +0530

    3

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://localhost:8800/api";
-// const API_URL = "https://personal-tm.onrender.com";
+// const API_URL = "http://localhost:8800/api";
+const API_URL = "https://personal-tm.onrender.com";
 
 
 // const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
