4a3124fb23a514cea88c81bb7f5769e16fa12078
Saketkumar24 <saketc112@gmail.com>
2025-04-24 20:07:43 +0530

    vercel

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
 const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "7d",
   });
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true, // Use secure cookies in production
-    samesite: "none",
-   maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
+    secure: true, // required for HTTPS
+    sameSite: "None", // important for cross-origin cookies
+    maxAge: 24 * 60 * 60 * 1000,
   });
 };
 
